54e5527eb4a6a62de2fad78490a18ef29e9cd5a9
Nishant Dixit <nishant@thedevelopers.co>
2017-05-01 17:42:02 +0530

    First Commit with the Menu Done

---
+++ inc/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/bootstrap.css" />
+	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/custom.css"/>
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse navbar-custom">
+		<div class="container">
+
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navHeaderCollapse" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar top-bar"></span>
+					<span class="icon-bar middle-bar"></span>
+					<span class="icon-bar bottom-bar"></span>
+				</button>
+				<a class="navbar-brand brand" href="#"><img class="" src="inc/images/logo.png" width="60" height="60"></a>
+			</div>
+			<div class="collapse navbar-collapse" id="navHeaderCollapse">
+				<ul class="nav navbar-nav navbar-right navbar-item-custom">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+
+	<!-- Jquery and the Bootstrap JS goes Here -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="inc/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>

69a9804b973ee1e3cee6edd5c24face9eb4cbfd2
Nishant Dixit <nishant@thedevelopers.co>
2017-05-05 17:46:06 +0530

    Final changes before BS ver 4

--- inc/header.php
+++ inc/header.php
@@ -4,12 +4,13 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Source+Sans+Pro" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/bootstrap.css" />
 	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/custom.css"/>
 </head>
 
 <body>
-	<nav class="navbar navbar-inverse navbar-custom">
+	<nav class="navbar navbar-inverse navbar-custom navbar-static-top">
 		<div class="container">
 
 			<div class="navbar-header">
@@ -19,12 +20,22 @@
 					<span class="icon-bar middle-bar"></span>
 					<span class="icon-bar bottom-bar"></span>
 				</button>
-				<a class="navbar-brand brand" href="#"><img class="" src="inc/images/logo.png" width="60px" height="60px"><span id="logo">Digital Work Studio</span></a>
+				<a class="navbar-brand brand" href="#"><img class="custom-logo" src="inc/images/logo.png"><span id="logo">Digital Work Studio</span></a>
 			</div>
 			<div class="collapse navbar-collapse" id="navHeaderCollapse">
 				<ul class="nav navbar-nav navbar-right navbar-item-custom">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Services</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="printing.php">Printing</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="scanning.php">Scanning</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="editing.php">Editing/Post Production</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="m&l.php">Mounting & Lamination</a></li>
+					</ul>
+					</li>
 					<li><a href="#">About</a></li>
 					<li><a href="#">Contact</a></li>
 				</ul>
@@ -35,8 +46,8 @@
 
 
 	<!-- Jquery and the Bootstrap JS goes Here -->
-
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="inc/bootstrap/js/bootstrap.js"></script>
+	<script src="inc/custom.js"></script>
 </body>
 </html>
---
+++ printing.php
@@ -0,0 +1,16 @@
+<?php require_once('inc/header.php') ?>
+<div class="container">
+    <div class="row sub-service">
+        <div class="col-md-6 service-container printer-sub-1">
+        
+        </div>
+        <div class="col-md-2 grey service-container printer-sub-2">
+            <p>At Digital Work Studios, we take your print as seriously as you can fathom. Our pioneer printing technologies ensure that you get the best print everytime. Nobody provides the same continuous tone of photographic prints as we do on specially crafted and ordered archival papers.</p>
+            <p>We are continuouslly raising the bar in the field of photography imagery and our fully equipped modern labs maintains quality of your prints from LVT film output, fine art retouching, digital slides and archival digital C - Prints and more</p>
+            <p><a href="printing-gallery.php">See Gallery >></a></p>
+        </div>
+        <div class="col-md-3 grey service-container printer-sub-3">
+        
+        </div>
+    </div>
+</div>

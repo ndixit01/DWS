1c3c3f235c677a38688715b3328dd609ac9ac83a
Nishant Dixit <nishant@thedevelopers.co>
2017-05-06 00:22:46 +0530

    Made Bootstrap 4 compatible

--- inc/header.php
+++ inc/header.php
@@ -7,40 +7,35 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway|Source+Sans+Pro" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/bootstrap.css" />
 	<link type="text/css" rel="stylesheet" href="inc/bootstrap/css/custom.css"/>
+    <link type="text/css" rel="stylesheet" href="inc/bootstrap/css/bootstrap-grid.css" />
+    <link type="text/css" rel="stylesheet" href="inc/bootstrap/css/bootstrap-reboot.css" />
 </head>
 
 <body>
-	<nav class="navbar navbar-inverse navbar-custom navbar-static-top">
-		<div class="container">
-
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navHeaderCollapse" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar top-bar"></span>
-					<span class="icon-bar middle-bar"></span>
-					<span class="icon-bar bottom-bar"></span>
-				</button>
-				<a class="navbar-brand brand" href="#"><img class="custom-logo" src="inc/images/logo.png"><span id="logo">Digital Work Studio</span></a>
-			</div>
-			<div class="collapse navbar-collapse" id="navHeaderCollapse">
-				<ul class="nav navbar-nav navbar-right navbar-item-custom">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="printing.php">Printing</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="scanning.php">Scanning</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="editing.php">Editing/Post Production</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="m&l.php">Mounting & Lamination</a></li>
-					</ul>
+	<nav class="navbar navbar-toggleable-lg navbar-light" style="background-color: transparent;">
+		<div class="container col-md-9">
+            <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerHead" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar top-bar"></span>
+                <span class="icon-bar middle-bar"></span>
+                <span class="icon-bar bottom-bar"></span>
+            </button>
+            <a class="navbar-brand d-flex w-25 mr-0" href="index.php"><img class="custom-logo" src="inc/images/logo.png"><span id="logo">Digital Work Studio</span></a>
+            <div class="collapse navbar-collapse" id="navbarTogglerHead">                
+                <ul class="navbar-nav mx-auto w-100 justify-content-end">
+					<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+					<li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="printing.php">Printing</a>
+						<a class="dropdown-item" href="scanning.php">Scanning</a>
+						<a class="dropdown-item" href="editing.php">Editing/Post Production</a>
+						<a class="dropdown-item" href="m&l.php">Mounting & Lamination</a>
+					</div>
 					</li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Contact</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">About</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
 				</ul>
-			</div>
-		</div>
+            </div>
+        </div>
 	</nav>
 
 
--- printing.php
+++ printing.php
@@ -1,16 +1,50 @@
 <?php require_once('inc/header.php') ?>
-<div class="container">
+<div class="container col-md-9">
     <div class="row sub-service">
-        <div class="col-md-6 service-container printer-sub-1">
-        
+        <div class="col-md-6 service-container printer-sub-1 p-0">
+            <div class="service-mask-head">
+                <div class="service-mask">
+                    <div class="service-mask w-100 d-flex justify-content-center align-items-center flex-column" onclick="location.href='printing-gallery.php'">
+                        <img class="img-fluid" src="inc/images/zoom.png">
+                        <span>See Gallery</span>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-2 grey service-container printer-sub-2">
+        <div class="col-md-2 service-container printer-sub-2">
             <p>At Digital Work Studios, we take your print as seriously as you can fathom. Our pioneer printing technologies ensure that you get the best print everytime. Nobody provides the same continuous tone of photographic prints as we do on specially crafted and ordered archival papers.</p>
             <p>We are continuouslly raising the bar in the field of photography imagery and our fully equipped modern labs maintains quality of your prints from LVT film output, fine art retouching, digital slides and archival digital C - Prints and more</p>
             <p><a href="printing-gallery.php">See Gallery >></a></p>
         </div>
-        <div class="col-md-3 grey service-container printer-sub-3">
-        
+        <div class="col-md-3 d-flex align-content-between flex-wrap service-container">
+            <div class="printer-sub-3-1">
+                <div class="service-mask-head">
+                    <div class="service-mask">
+                        <div class="service-mask w-100 d-flex justify-content-center align-items-center">
+                            <img src="inc/images/zoom.png">
+                            <span>See Gallery</span>
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="printer-sub-3-2">
+                <div class="service-mask-head">
+                    <div class="service-mask">
+                        <div class="service-mask w-100 d-flex justify-content-center align-items-center">
+                            <img src="inc/images/zoom.png">
+                            <span>See Gallery</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+<script type="text/javascript">
+$(document).ready(function(){
+	$('.service-mask').hide();
+	$('.service-mask-head').hover(function(){
+		$(this).children('.service-mask').fadeToggle(200, "linear" );		
+	});
+});
+</script>
